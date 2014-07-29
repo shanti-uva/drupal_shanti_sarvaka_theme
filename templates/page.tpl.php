c9e982fcd84927ba8b47e476b502504d20aff0ed
mferrara <mf8yk@virginia.edu>
2014-07-29 13:44:04 -0500

    update side column classes, thumb shadow

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -73,7 +73,7 @@
           <?php if (isset($messages)) { ?><section class="messages"><?php print $messages; ?></section> <?php } ?>
           <!-- Sidebar First Region -->
           <?php if ($page['sidebar_first']): ?>
-            <div id="sidebar-first" class="region sidebar col-xs-6 col-sm-3 sidebar-offcanvas <?php print $bsclass_sb1; ?>">
+            <div id="sidebar-first" class="region sidebar sidebar-offcanvas col-xs-6 <?php print $bsclass_sb1; ?>">
               <?php print render($page['sidebar_first']); ?>
             </div>
           <?php endif; ?>
@@ -90,7 +90,7 @@
           
           <!-- Sidebar Second Region -->
           <?php if ($page['sidebar_second']): ?>
-            <div id="sidebar-second" class="region sidebar col-xs-6 col-sm-3 sidebar-offcanvas <?php print $bsclass_sb2; ?>">
+            <div id="sidebar-second" class="region sidebar pull-right sidebar-offcanvas col-xs-6 <?php print $bsclass_sb2; ?>">
               <?php print render($page['sidebar_second']); ?>
             </div>
           <?php endif; ?>
