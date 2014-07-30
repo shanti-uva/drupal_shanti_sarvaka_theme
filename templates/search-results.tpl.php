79d097ca97157137b89d04f7da94be0147c5c0dd
mferrara <mf8yk@virginia.edu>
2014-07-30 14:53:27 -0500

    minor css, remove ol, select picker in main.css

--- templates/search-results.tpl.php
+++ templates/search-results.tpl.php
@@ -25,9 +25,9 @@
 ?>
 <?php if ($search_results): ?>
   <h2><?php print t('Search results');?></h2>
-  <ol class="search-results <?php print $module; ?>-results">
+  <section class="search-results <?php print $module; ?>-results">
     <?php print $search_results; ?>
-  </ol>
+  </section>
   <?php print $pager; ?>
 <?php else : ?>
   <h2><?php print t('Your search yielded no results');?></h2>
