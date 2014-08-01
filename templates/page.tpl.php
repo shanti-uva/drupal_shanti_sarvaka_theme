1ba6e09c5c35b87f9c6bd6aed681598ae2844002
mferrara <mf8yk@virginia.edu>
2014-07-31 19:08:10 -0500

    div on message, remove justify, carousel css
    changed section tag to div for the message container, semantics
    considered

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -70,7 +70,7 @@
         
         <div class="row row-offcanvas row-offcanvas-right">
           <!-- Message Area -->
-          <?php if (isset($messages)) { ?><section class="messages"><?php print $messages; ?></section> <?php } ?>
+          <?php if (isset($messages)) { ?><div class="messages"><?php print $messages; ?></div> <?php } ?>
           <!-- Sidebar First Region -->
           <?php if ($page['sidebar_first']): ?>
             <div id="sidebar-first" class="region sidebar col-xs-6 <?php print $bsclass_sb1; ?>">
