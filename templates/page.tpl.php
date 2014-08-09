cab64e2e27ac1556da5f0afa12ff04418ef1c9f5
mferrara <mf8yk@virginia.edu>
2014-08-09 10:31:41 -0500

    misc changes for links, buttons, indent navbar

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -3,52 +3,55 @@
     <!-- Header Region -->
    <header class="header-banner">
     <div class="navbar navbar-default" role="navigation"> 
-     
-      <nav class="navbar-buttons">
-        <span class="menu-icon shanti-searchtoggle"><a href="#"><i class='icon shanticon-search'></i></a></span><!-- mobile < 400 : search -->
-        <span class="menu-icon menu-toggle"><a href="#"><i class="icon shanticon-menu"></i></a></span><!-- desktop > 768 drilldown menu : main-menu -->
-        <span class="menu-icon menu-maintoggle"><a href="#"><i class="icon shanticon-menu"></i></a></span><!-- mobile < 768 : main-menu -->
-        <span class="menu-explore menu-exploretoggle"><a href="#"><span>Explore </span>Collections<i class="icon shanticon-directions"></i></a></span><!-- mobile < 768 : collections -->
-      </nav>
-
-
-        <h1 class="navbar-header<?php if(!$variables['shanti_site']) { print " default"; } ?>">
-          <a href="<?php print $variables['home_url']; ?>" class="navbar-brand" title="<? print $site_name; ?> Homepage">
-            <?php if($variables['shanti_site']): ?>
-              <i class="icon shanticon-logo"></i><em>SHANTI</em>
-            <?php else: ?>
-              <img src="<?php print $logo; ?>" class="site-logo" /> <span class="site-title"><?php print $site_name; ?></span>
-            <?php endif; ?>
-            <?php if($site_slogan) { print '<span class="site-slogan">' . $site_slogan . '</span>' ;} ?>
-          </a>
-        </h1>
-  
-      <!-- HEADER REGION -->
-      <nav id="sarvaka-header" class="region navbar-collapse collapse navtop"> <!-- desktop display > 768 -->
-         <form class="form">
-         <fieldset>         
-          <ul class="nav navbar-nav navbar-right">
-            <?php if(module_exists('explore_menu')): ?>
-              <li id="collexplink" class="explore closed"><a href="#"><?php print $variables['explore_menu_link']; ?> 
-                <i class="icon shanticon-directions"></i></a></li>
-            <?php endif; ?>
-            <?php  if(isset($variables['language_switcher'])) { print $variables['language_switcher']; }  ?>
-            <!-- TODO: Need to hardcode user menu/options here as well -->
-            <!-- If admin puts blocks in  header, render here -->
-            <?php print render($page['header']);  ?>
-          </ul>
-         </fieldset>  
-         </form>           
-       </nav>
-       <!-- End of HEADER region -->
+	     
+	      <nav class="navbar-buttons">
+	        <span class="menu-icon shanti-searchtoggle"><a href="#"><i class='icon shanticon-search'></i></a></span><!-- mobile < 400 : search -->
+	        <span class="menu-icon menu-toggle"><a href="#"><i class="icon shanticon-menu"></i></a></span><!-- desktop > 768 drilldown menu : main-menu -->
+	        <span class="menu-icon menu-maintoggle"><a href="#"><i class="icon shanticon-menu"></i></a></span><!-- mobile < 768 : main-menu -->
+	        <span class="menu-explore menu-exploretoggle"><a href="#"><span>Explore </span>Collections<i class="icon shanticon-directions"></i></a></span><!-- mobile < 768 : collections -->
+	      </nav>
+	
+	      <h1 class="navbar-header<?php if(!$variables['shanti_site']) { print " default"; } ?>">
+	        <a href="<?php print $variables['home_url']; ?>" class="navbar-brand" title="<? print $site_name; ?> Homepage">
+	          <?php if($variables['shanti_site']): ?>
+	            <i class="icon shanticon-logo"></i><em>SHANTI</em>
+	          <?php else: ?>
+	            <img src="<?php print $logo; ?>" class="site-logo" /> <span class="site-title"><?php print $site_name; ?></span>
+	          <?php endif; ?>
+	          <?php if($site_slogan) { print '<span class="site-slogan">' . $site_slogan . '</span>' ;} ?>
+	        </a>
+	      </h1>
+	  
+	      <!-- HEADER REGION -->
+	      <nav id="sarvaka-header" class="region navbar-collapse collapse navtop"> <!-- desktop display > 768 -->
+	         <form class="form">
+	         <fieldset>         
+	          <ul class="nav navbar-nav navbar-right">
+	            <?php if(module_exists('explore_menu')): ?>
+	              <li id="collexplink" class="explore closed"><a href="#"><?php print $variables['explore_menu_link']; ?> 
+	                <i class="icon shanticon-directions"></i></a></li>
+			            <?php endif; ?>
+			            <?php  if(isset($variables['language_switcher'])) { print $variables['language_switcher']; }  ?>
+			            
+			            <!-- TODO: Need to hardcode user menu/options here as well -->
+			            <!-- If admin puts blocks in  header, render here -->
+			            <?php print render($page['header']);  ?>
+	          </ul>
+	         </fieldset>  
+	         </form>           
+	       </nav>
+	       <!-- End of HEADER region -->
      </div>
      <!-- include shanti-explore-menu if it exists -->
      <?php if(module_exists('explore_menu')) { print render($variables['explore_menu']); } ?>
     </header>
     
+    
+    <!-- Begin Content Region -->
     <main class="main-wrapper container">
       <article class="main-content" role="main">
         <div class="row">
+          
           <!-- Banner Region -->
           <header class="col-sm-12 titlearea banner">
            <div role="banner">
@@ -66,8 +69,11 @@
               ?>
             </div>
           </header>
+          
         </div> <!-- End of Banner Region -->
         
+       
+        <!-- Begin Content Row -->
         <div class="row row-offcanvas row-offcanvas-right">
           
           <!-- Sidebar First Region -->
@@ -96,6 +102,7 @@
             </div>
           <?php endif; ?>
         </div>
+        
         <a href="#" class="back-to-top"><i class="icon"></i></a>
       </article>
 
@@ -122,6 +129,8 @@
   </section><!-- END menu -->
 </div> <!-- End wrap-all -->
 
+
+
 <!-- Footer -->
 <footer class="footer">
   <div>
@@ -134,3 +143,7 @@
 <div id="admin-footer">
   <?php print render($page['admin_footer']); ?> 
 </div>
+
+
+
+
