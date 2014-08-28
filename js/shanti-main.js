bd58925e393b427e0c53d47a7289ed3a34c4eeb9
mferrara <mf8yk@virginia.edu>
2014-08-28 18:03:50 -0500

    js on hover for thumb

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -674,10 +674,10 @@ jQuery(function ($) {
   $('.shanti-field-group-audience > div').find('a:eq(1)').addClass('icon-link');
   
   $('.shanti-field-title a').hover( function() {
-	      $(this).parent().find('.shanti-thumbnail-image').addClass('on-hover');
+	      $(this).parent('.shanti-thumbnail').addClass('title-hover');
 	      },
 	        function () {
-	      $(this).parent().find('.shanti-thumbnail-image').removeClass('on-hover');
+	      $(this).parent('.shanti-thumbnail').removeClass('title-hover');
 	      }
 	 );
 	 
