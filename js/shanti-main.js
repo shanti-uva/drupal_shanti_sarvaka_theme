54ecadcb427272e8fba8c72e5e75caadb57ac37b
mferrara <mf8yk@virginia.edu>
2014-08-31 15:35:12 -0500

    update gallery grid testing

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -714,7 +714,7 @@ jQuery(function ($) {
           itemWidth: 180, // Optional min width of a grid item
           autoResize: true, // This will auto-update the layout when the browser window is resized.
           container: $('.shanti-gallery'), // Optional, used for some extra CSS styling
-          offset: 5, // Optional, the distance between grid items
+          offset: 15, // Optional, the distance between grid items
           outerOffset: 20, // Optional the distance from grid to parent
           flexibleWidth: '50%' // Optional, the maximum width of a grid item
         };
