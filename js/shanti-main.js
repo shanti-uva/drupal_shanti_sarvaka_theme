3288b3a189d935f41f4ac909d021006c50894ba9
mferrara <mf8yk@virginia.edu>
2014-08-31 14:50:05 -0500

    new gallery testing, bookmark

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -706,6 +706,45 @@ jQuery(function ($) {
  
  
  
+jQuery(function ($) {
+    (function ($){
+      $('.shanti-gallery').imagesLoaded(function() {
+        // Prepare layout options.
+        var options = {
+          itemWidth: 180, // Optional min width of a grid item
+          autoResize: true, // This will auto-update the layout when the browser window is resized.
+          container: $('.shanti-gallery'), // Optional, used for some extra CSS styling
+          offset: 5, // Optional, the distance between grid items
+          outerOffset: 20, // Optional the distance from grid to parent
+          flexibleWidth: '50%' // Optional, the maximum width of a grid item
+        };
+
+        // Get a reference to your grid items.
+        var handler = $('.shanti-gallery li');
+
+        var $window = $(window);
+        $window.resize(function() {
+          var windowWidth = $window.width(),
+              newOptions = { flexibleWidth: '50%' };
+
+          // Breakpoint
+          if (windowWidth < 1024) {
+            newOptions.flexibleWidth = '100%';
+          }
+
+          handler.wookmark(newOptions);
+        });
+
+        // Call the layout function.
+        handler.wookmark(options);
+      });
+    })(jQuery);
+});
+
+ 
+ 
+ 
+ 
 
 
 jQuery(function ($) {
