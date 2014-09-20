a61cf20f20fed952a7490eab8c6fbdd8cbb6d84d
Edward Garrett <edward@pinedrop.com>
2014-09-20 23:24:56 +0100

    using render arrays in template

--- templates/transcripts-interactive-transcript.tpl.php
+++ templates/transcripts-interactive-transcript.tpl.php
@@ -1,16 +1,15 @@
 <div>
 	<div id='<?php print $trid; ?>' class='transcript-player' data-defaultMode='<?php print $default_mode; ?>' data-hello='<?php print $hello; ?>'>
 		<table>
-		<!-- <table class='sticky-header' style='position: fixed; top: 0px; left: 25px; visibility: visible; width: 867px;'> -->
 			<tr>
 				<td class='v-wrap' style='padding-right: 20px;'>
 					<?php print $video_tag; ?>
-					<?php print $video_controls; ?>
-					<?php print $hits; ?>
+					<?php print render($video_controls); ?>
+					<?php print render($hits); ?>
 				</td>
 				<td class='t-wrap'>
-					<?php print $transcript_controls; ?>
-					<?php print $transcript; ?>
+					<?php print render($transcript_controls); ?>
+					<?php print render($transcript); ?>
 				</td>
 			</tr>
 		</table>
