2902bcc2b04927cae2d378cd8f7a77ff7c07a032
Edward Garrett <edward@pinedrop.com>
2014-09-27 18:27:51 +0100

    moved transcript search results to right of video

--- templates/transcripts-interactive-transcript.tpl.php
+++ templates/transcripts-interactive-transcript.tpl.php
@@ -5,11 +5,15 @@
 				<td class='v-wrap' style='padding-right: 20px;'>
 					<?php print $video_tag; ?>
 					<?php print render($video_controls); ?>
-					<?php print render($hits); ?>
 				</td>
 				<td class='t-wrap'>
 					<?php print render($transcript_controls); ?>
-					<?php print render($transcript); ?>
+                                        <?php if (count($hits)): ?>
+                                                <?php print render($hits); ?>
+                                                <div style='display:none;'?><?php print render($transcript); ?></div>
+                                        <?php else: ?>
+                                                <?php print render($transcript); ?>
+                                        <?php endif; ?>
 				</td>
 			</tr>
 		</table>
