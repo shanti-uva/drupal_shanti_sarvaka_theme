9a018be5f2948e7411eba6c72d2322d6eb00b8e9
mferrara <mf8yk@virginia.edu>
2014-11-11 17:51:54 -0600

    new classes on menu section wrapper
    the 3 classes are all that was added, my editor is removing extra white
    space on the other lines

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -2,15 +2,15 @@
    <span class="sr-only"><a href=".main-content">Skip to main content</a> <a href="#kmaps-search">Skip to search</a></span>
     <!-- Header Region -->
    <header class="header-banner">
-    <div class="navbar navbar-default" role="navigation"> 
-	     
+    <div class="navbar navbar-default" role="navigation">
+
 	      <nav class="navbar-buttons">
 	        <span class="menu-icon shanti-searchtoggle"><a href="#"><i class='icon shanticon-search'></i></a></span><!-- mobile < 400 : search -->
 	        <span class="menu-icon menu-toggle"><a href="#"><i class="icon shanticon-menu"></i></a></span><!-- desktop > 768 drilldown menu : main-menu -->
 	        <span class="menu-icon menu-maintoggle"><a href="#"><i class="icon shanticon-menu"></i></a></span><!-- mobile < 768 : main-menu -->
 	        <span class="menu-explore menu-exploretoggle"><a href="#"><span>Explore </span>Collections<i class="icon shanticon-directions"></i></a></span><!-- mobile < 768 : collections -->
 	      </nav>
-	
+
 	      <h1 class="navbar-header<?php if(!$variables['shanti_site']) { print " default"; } ?>">
 	        <a href="<?php print $variables['home_url']; ?>" class="navbar-brand" title="<?php print $site_name; ?> Homepage">
 	          <?php if($variables['shanti_site']): ?>
@@ -27,41 +27,41 @@
 	      <!-- HEADER REGION -->
 	      <nav id="sarvaka-header" class="region navbar-collapse collapse navtop"> <!-- desktop display > 768 -->
 	         <form class="form">
-	         <fieldset>         
+	         <fieldset>
 	          <ul class="nav navbar-nav navbar-right">
 	            <?php if(module_exists('explore_menu')): ?>
-	              <li id="collexplink" class="explore closed"><a href="#"><?php print $variables['explore_menu_link']; ?> 
+	              <li id="collexplink" class="explore closed"><a href="#"><?php print $variables['explore_menu_link']; ?>
 	                <i class="icon shanticon-directions"></i></a></li>
 			            <?php endif; ?>
 			            <?php  if(isset($variables['language_switcher'])) { print $variables['language_switcher']; }  ?>
-			            
+
 			            <!-- TODO: Need to hardcode user menu/options here as well -->
 			            <!-- If admin puts blocks in  header, render here -->
 			            <?php print render($page['header']);  ?>
 	          </ul>
-	         </fieldset>  
-	         </form>           
+	         </fieldset>
+	         </form>
 	       </nav>
 	       <!-- End of HEADER region -->
      </div>
      <!-- include shanti-explore-menu if it exists -->
      <?php if(module_exists('explore_menu')) { print render($variables['explore_menu']); } ?>
     </header>
-    
-    
+
+
     <!-- Begin Content Region -->
     <main class="main-wrapper container">
       <article class="main-content" role="main">
         <div class="row">
-          
+
           <!-- Banner Region -->
           <header class="col-xs-12 titlearea banner<?php print $variables['banner_class']; ?>">
            <div role="banner">
-            <h1 class="page-title"><i class="icon shanticon-<?php print $variables['icon_class']; ?>"></i><span><?php 
+            <h1 class="page-title"><i class="icon shanticon-<?php print $variables['icon_class']; ?>"></i><span><?php
             	if(!empty($variables['default_title']) && !empty($variables['prefix_default_title'])) {
             		print ($title == '')? $variables['default_title'] : $variables['default_title'] . ': ' . $title;
             	} else {
-            		print ($title == '')? $variables['default_title']:$title; 
+            		print ($title == '')? $variables['default_title']:$title;
             	}
               ?></span></h1>
               <nav class="breadwrap" role="navigation">
@@ -70,28 +70,28 @@
               <div class="banner-content">
                 <?php print render($page['banner']); ?>
               </div>
-              <?php 
+              <?php
                 // For view/edit tabs
-                print render($tabs); 
+                print render($tabs);
               ?>
             </div>
           </header>
-          
+
         </div> <!-- End of Banner Region -->
-        
-       
+
+
         <!-- Begin Content Row -->
         <div class="row row-offcanvas row-offcanvas-right">
-          
+
           <!-- Sidebar First Region -->
           <?php if ($page['sidebar_first']): ?>
             <div id="sidebar-first" class="region sidebar col-xs-6 <?php print $bsclass_sb1; ?>">
               <?php print render($page['sidebar_first']); ?>
             </div>
           <?php endif; ?>
-          
+
           <!-- Begin Page Content -->
-          <section class="content-section col-xs-12<?php if (!empty($bsclass_main)) { print ' ' . $bsclass_main; } ?>"> 
+          <section class="content-section col-xs-12<?php if (!empty($bsclass_main)) { print ' ' . $bsclass_main; } ?>">
           <!-- Message Area -->
           <?php if (!empty($messages)) { print "<div class=\"messages\">$messages</div>"; } ?>
             <div class="tab-content">
@@ -102,7 +102,7 @@
             </div>
           </section>
           <!-- END Content -->
-          
+
           <!-- Sidebar Second Region -->
           <?php if ($page['sidebar_second']): ?>
             <div id="sidebar-second" class="region sidebar pull-right sidebar-offcanvas col-xs-6 <?php print $bsclass_sb2; ?>">
@@ -110,27 +110,27 @@
             </div>
           <?php endif; ?>
         </div>
-        
+
         <a href="#" class="back-to-top"><i class="icon fa"></i></a>
       </article>
 
 		  <!-- Search Flyout -->
 		  <?php if(!empty($page['search_flyout'])): ?>
 		      <!--print render($page['search_flyout']); -->
-				<div id="search-flyout" class="region extruder right" role="search" style="display: none;"> 
+				<div id="search-flyout" class="region extruder right" role="search" style="display: none;">
 				   <?php print render($page['search_flyout']); ?>
 				</div>
 	    <?php endif; ?>
 
     </main> <!-- End Main Content -->
 
-    
-  <!-- LOAD menus -->
-  <section id="menu-main" class="{ url:'<?php if(!empty($base_theme_path)) { print $base_theme_path; } else { print $theme_path; } ?>js/inc/menus/menu-ajax.php'} menu-accordion">   </section>  
-  <section id="menu-collections" class="{ url:'<?php if(!empty($base_theme_path)) { print $base_theme_path; } else { print $theme_path; } ?>js/inc/menus/menu-ajax.php'} menu-accordion">    </section>
 
-  <section id="menu" style="display:none;">
-    <nav id="menu-drill" role="navigation">                
+  <!-- LOAD menus -->
+  <section id="menu-main" class="menu-main-mobile { url:'<?php if(!empty($base_theme_path)) { print $base_theme_path; } else { print $theme_path; } ?>js/inc/menus/menu-ajax.php'} menu-accordion">   </section>
+  <section id="menu-collections" class="menu-collections-mobile { url:'<?php if(!empty($base_theme_path)) { print $base_theme_path; } else { print $theme_path; } ?>js/inc/menus/menu-ajax.php'} menu-accordion">    </section>
+
+  <section id="menu" class="menu-main-desk" style="display:none;">
+    <nav id="menu-drill" role="navigation">
      <?php print $variables['user_menu_links']; ?>
     </nav>
   </section><!-- END menu -->
@@ -146,7 +146,7 @@
 
 <!-- Admin Footer -->
 <div id="admin-footer">
-  <?php print render($page['admin_footer']); ?> 
+  <?php print render($page['admin_footer']); ?>
 </div>
 
 
