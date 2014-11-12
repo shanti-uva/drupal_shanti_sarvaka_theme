ee423ef2ea18e94836e0c7ee411dafca78415e28
ThanGrove <than@virginia.edu>
2014-11-12 14:44:07 -0500

    styles and image for ajax loading gif

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -455,6 +455,14 @@
   Drupal.behaviors.shanti_sarvaka_miscinit = {
     attach: function (context, settings) {
     	if(context == document) {
+    		
+	      // Shanti-filters title keyword search field: use description for placeholder text
+    		$('.shanti-filters .views-exposed-form .form-item input.form-text').each(function() { 
+    			var desc = $(this).parent().parent().next('.description'); 
+    			$(this).attr({'placeholder': $.trim(desc.text()), 'size':'15'}); 
+    			desc.remove();
+    		});
+    		
 	      // *** GLOBAL ** conditional IE message
 	      // show-hide the IE message for older browsers
 	      // this could be improved with conditional for - lte IE7 - so it does not self-hide
@@ -527,6 +535,7 @@
 	          $(this).toggleClass('paused');
 	          $(this).find('i').toggleClass('glyphicon-pause glyphicon-play');
 	      });
+	      
 	    }
     }
   };
