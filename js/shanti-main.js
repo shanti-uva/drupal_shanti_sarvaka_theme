b57efa596364174173117d539afc2efc2def83dc
mferrara <mf8yk@virginia.edu>
2015-02-16 16:44:05 -0600

    off canvas js move to main

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -748,5 +748,26 @@
 	  }
 	};
   
+  
+  
+  
+  Drupal.behaviors.kmapsOffCanvasToggle = {
+	  attach: function (context, settings) {
+			$("button.view-resources").click( function() { 		// show-hide resource side-column
+			        $(this).toggleClass( "show",'fast' );
+			});
+	  }
+	};
+	
+	
+	
+	Drupal.behaviors.kmapsOffCanvasButton = {
+	  attach: function (context, settings) {
+			if($(".feature-carousel-tabpanel").length ) {
+				$(".btn-default.view-resources").css('display','none !important');
+			}
+	  }
+	};
+  
   		
 }(jQuery));
