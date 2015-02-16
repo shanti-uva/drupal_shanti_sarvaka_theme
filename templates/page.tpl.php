a94990bc6494c11e2febf0040473d02fa002ad09
ThanGrove <than@virginia.edu>
2015-02-16 16:29:46 -0500

    update to side col and content bs col settings to use preprocess_page variables

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -81,13 +81,13 @@
 
           <!-- Sidebar First Region -->
           <?php if ($page['sidebar_first']): ?>
-            <div id="sidebar-first" class="region sidebar col-xs-6 col-sm-3 col-lg-3 <?php print $bsclass_sb1; ?>">
+            <div id="sidebar-first" class="region sidebar<?php print " $bsclass_sb1"; ?>">
               <?php print render($page['sidebar_first']); ?>
             </div>
           <?php endif; ?>
 
           <!-- Begin Page Content -->
-          <section class="col-xs-12<?php if (!empty($bsclass_main)) { print ' ' . $bsclass_main; } ?>">
+          <section class="col-xs-12<?php if (!empty($bsclass_main)) { print " $bsclass_main"; } ?>">
 	        
 	        <button type="button" class="btn btn-default visible-sm view-resources" data-toggle="offcanvas">
             <i class="fa"></i>
@@ -109,7 +109,7 @@
 
           <!-- Sidebar Second Region -->
           <?php if ($page['sidebar_second']): ?>
-            <div id="sidebar-second" class="region sidebar pull-right sidebar-offcanvas col-xs-6 col-sm-3 col-lg-3 <?php print $bsclass_sb2; ?>">
+            <div id="sidebar-second" class="region sidebar pull-right sidebar-offcanvas<?php print " $bsclass_sb2"; ?>">
               <?php print render($page['sidebar_second']); ?>
             </div>
           <?php endif; ?>
