8974781a3b6efcad076a738f49e63dc28d329a16
mferrara <mf8yk@virginia.edu>
2015-03-26 14:54:03 -0600

    flyout height

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -19,7 +19,7 @@
   Drupal.ShantiSarvaka.searchTabHeight = function() {
     var height = $(window).height();
     var srchtab = (height) - 80;
-    var viewheight = (height) - 243;
+    var viewheight = (height) - 253;
     // var advHeight = $(".advanced-view").show().height();
     var comboHeight = (viewheight) - 142;
 
