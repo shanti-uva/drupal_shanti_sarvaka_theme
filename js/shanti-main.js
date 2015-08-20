70f2f671e01e57dddf427406b75c374830117ba6
dukarla <marco@vpn-user-1-200.itc.Virginia.EDU>
2015-08-19 21:27:14 -0500

    fixed carousel text trim for carousels

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -667,7 +667,7 @@
       if(context == window.document) {                
         $('.carousel-description p').each(function() { 
           var txt = $(this).text();
-          if ($(this).text().length > 300) { 
+          if ($(this).text().length > 500) { 
             txt = txt.substr(0, 500);
             txt = txt.substr(0, txt.lastIndexOf(' ')) + "..."; 
             $(this).text(txt);
