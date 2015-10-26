4ce93f86d5d9ae78cbe2c01b946ab333182ebc9c
dukarla <artesano@vpn-user-1-173.itc.Virginia.EDU>
2015-10-26 14:53:21 -0600

    showing advanced search

--- templates/search-block-form.tpl.php
+++ templates/search-block-form.tpl.php
@@ -55,7 +55,7 @@
 
          <div id="notification-wrapper"></div>
 
-         <section class="advanced-view" style="display:none;">
+         <section class="advanced-view">
             <div class="form-group">
               <label class="radio-inline" for="radios-0">
                 <input type="radio" name="radios" id="radios-0" value="1" checked="checked">
