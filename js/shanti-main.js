71f6cdc6daca9db9f7e7df84b5fa99aaee3c2ac0
dukarla <artesano@vpn-user-1-73.itc.Virginia.EDU>
2015-10-29 21:03:47 -0600

    update search fly-out height for removing scrollbars

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -51,7 +51,7 @@
   Drupal.ShantiSarvaka.searchTabHeight = function() {    
     var height = $(window).height();
     var srchtab = (height) - 88;
-    var viewheight = (height) - 190;
+    var viewheight = (height) - 240;
     // var advHeight = $(".advanced-view").show().height();
     // var comboHeight = (viewheight) - 370;
 
