f3e919870bd0ccef443b6e5985a24b753bae6e86
dukarla <artesano@vpn-user-1-19.itc.Virginia.EDU>
2015-11-22 15:58:02 -0600

    layout test with BS class container-fluid on content wrapper

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -95,7 +95,7 @@
           	<?php if (!empty($messages)) { print "<div class=\"messages\">$messages</div>"; } ?>
           	
           	<!-- Main Content -->
-            <div class="tab-content">
+            <div class="tab-content container-fluid">
               <article class="tab-pane main-col active" id="tab-overview">
               	 <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
                  <?php print render($page['content']); ?>
