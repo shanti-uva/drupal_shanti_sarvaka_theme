65533b9d332a2cfa5bb78b2ae23fedd7453c6dd4
ThanGrove <than@virginia.edu>
2016-04-06 13:28:34 -0400

    removing form alter for av node forms always placing media group at top (MANU-2472)

--- template.php
+++ template.php
@@ -278,11 +278,6 @@ function shanti_sarvaka_form_alter(&$form, &$form_state, $form_id) {
 		$form['actions']['submit']['#value'] = ''; // This replaced by the icon code in shanti_sarvaka_button function
 		$form['actions']['submit']['#icon'] = 'glyphicon-chevron-right';
 	}
-	if(in_array($form_id, array('video_node_form','audio_node_form'))) {
-		$form['#groups']['group_media']->weight = -10;
-		$form['group_audience']['#weight'] = -5;
-		//dpm($form, 'a/v edit form');
-	}
 }
 
 
