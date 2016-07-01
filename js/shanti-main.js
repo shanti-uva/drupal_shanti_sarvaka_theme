b2a43328e2251444ae3fe963a4e1e2c2ce207e45
dukarla <Artesano@vpn-user-1-179.itc.virginia.edu>
2016-06-30 19:02:06 -0500

    move kmaps home breadcrumb to kmaps

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -697,21 +697,6 @@
     }
   };
 
- 
-  Drupal.behaviors.shantiKmapsHideBreadcrumbsSubjects = {
-    attach: function (context, settings) {
-      if(context == window.document) {
-
-        $( window ).load(function() {
-          if($(".front.page-subjects").length ) { 
-               $(this).find(".breadwrap").remove();
-          }
-        });
-
-      }
-    } 
-  };
-
 Drupal.behaviors.shantiDeleteButtonDisable = {
 	 attach: function (context, settings) {
 			if (context == document) {
