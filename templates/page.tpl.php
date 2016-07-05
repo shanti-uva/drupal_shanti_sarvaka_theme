ff38218d52458c32f381345ed5a9b67ad37f3bb8
dukarla <Artesano@vpn-user-1-204.itc.virginia.edu>
2016-07-04 21:07:09 -0500

    revert navbar-buttons

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -4,9 +4,9 @@
    <header class="header-banner">
     <div class="navbar navbar-default">
 
-	      <nav class="navbar-buttons btn-group" role="group">
-	        <button typ="button" class="menu-icon menu-toggle"><a href="#"><span class="sr-only">Main Menu</span><span class="icon shanticon-menu"></span></a></button><!-- desktop > 768 drilldown menu : main-menu -->
-	        <button typ="button" class="menu-explore menu-exploretoggle"><a href="#"><span>Explore</span><span class="icon shanticon-directions"></span></a></button><!-- mobile < 768 : collections -->
+	      <nav class="navbar-buttons">
+	        <span class="menu-icon menu-toggle"><a href="#"><span class="sr-only">Main Menu</span><span class="icon shanticon-menu"></span></a></span><!-- desktop > 768 drilldown menu : main-menu -->
+	        <span class="menu-explore menu-exploretoggle"><a href="#"><span>Explore</span><span class="icon shanticon-directions"></span></a></span><!-- mobile < 768 : collections -->
 	      </nav>
 
 	      <h1 class="navbar-header<?php if(!$variables['shanti_site']) { print " default"; } ?>">
