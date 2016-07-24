ef855324a537c5ff8e46bfd1be1b306fa9f1491c
dukarla <Artesano@vpn-user-1-120.itc.virginia.edu>
2016-07-24 11:27:10 -0500

    template element & aria updates

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,7 +1,7 @@
 <div class="wrap-all">
    <span class="sr-only"><a href=".main-content">Skip to main content</a> <a href="#search-flyout">Skip to search</a></span>
     <!-- Header Region -->
-   <header class="header-banner">
+   <div class="site-banner">
     <div class="navbar navbar-default">
 
 	      <nav class="navbar-buttons">
@@ -37,25 +37,28 @@
      </div>
      <!-- include shanti-explore-menu if it exists -->
      <?php if(module_exists('explore_menu')) { print render($variables['explore_menu']); } ?>
-    </header>
+    </div>
 
 
     <!-- Begin Content Region -->
     <main class="main-wrapper container-fluid">
       <article class="main-content" role="main">
-        <div class="row">
+        <section class="row">
 
           <!-- Banner Region -->
-          <header class="titlearea banner<?php print $variables['banner_class']; ?>">
-           <div role="banner">
-            <h1 class="page-title"><span class="icon shanticon-<?php print $variables['icon_class']; ?>"></span><span class="page-title-text">
-            <?php
-            	if(!empty($variables['default_title']) && !empty($variables['prefix_default_title'])) {
-            		print ($title == '')? $variables['default_title'] : $variables['default_title'] . ': ' . $title;
-            	} else {
-            		print ($title == '')? $variables['default_title']:$title;
-            	}
-              ?></span></h1>
+          <div class="titlearea banner<?php print $variables['banner_class']; ?>">
+           <div>
+             <header role="banner">
+                <h1 class="page-title"><span class="icon shanticon-<?php print $variables['icon_class']; ?>"></span><span class="page-title-text">
+                <?php
+                	if(!empty($variables['default_title']) && !empty($variables['prefix_default_title'])) {
+                		print ($title == '')? $variables['default_title'] : $variables['default_title'] . ': ' . $title;
+                	} else {
+                		print ($title == '')? $variables['default_title']:$title;
+                	}
+                  ?></span>
+                </h1>
+              </header>
               <nav class="breadwrap" style="display:none;">
                 <?php print theme('breadcrumb', array('breadcrumb' => $breadcrumb)); ?>
               </nav>
@@ -69,13 +72,13 @@
                 ?>
               </div>
             </div>
-          </header>
+          </div>
 
-        </div> <!-- End of Banner Row -->
+        </section> <!-- End of Banner Row -->
 
 
         <!-- Begin Content Row -->
-        <div class="row row-offcanvas<?php print " $offcanvas_trigger_sb"; ?>">
+        <section class="row row-offcanvas<?php print " $offcanvas_trigger_sb"; ?>">
 
           <!-- Sidebar First Region -->
           <?php if ($page['sidebar_first']): ?>
@@ -110,9 +113,9 @@
               <?php print render($page['sidebar_second']); ?>
             </section>
           <?php endif; ?>
-        </div>
+        </section>
 
-        <a href="#" class="back-to-top"><span class="icon fa"></span></a>
+        <a href="#" class="back-to-top" role="button"><span class="icon fa"></span></a>
       </article>
 
 		  <!-- Search Flyout -->
