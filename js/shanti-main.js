9e06484370d9133792da8497bd8b754aef67c019
dukarla <Artesano@vpn-user-1-67.itc.virginia.edu>
2016-08-10 11:58:00 -0500

    testing scroll height for search flyout

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -53,7 +53,7 @@
     var srchtab = (height) - 88;
     var srchtabAdmin = (height) - 123; // subtract 35px height of Drupal admin navbar
     // var viewheight = (height) - 235;
-    var viewheight = (height) - 270;
+    var viewheight = (height) - 255;
     var viewheightSources = (height) - 230;
     var viewheightPlaces = (height) - 340;
     // var advHeight = $(".advanced-view").show().height();
