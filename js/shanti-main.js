0b8a35375b616a3c26e86f5a75b5f026498137e1
dukarla <Artesano@vpn-user-1-67.itc.virginia.edu>
2016-08-10 18:34:19 -0500

    testing scroll height for search flyout

--- js/shanti-main.js
+++ js/shanti-main.js
@@ -55,7 +55,7 @@
     // var viewheight = (height) - 235;
     var viewheight = (height) - 270;
     var viewheightSources = (height) - 230;
-    var viewheightPlaces = (height) - 355;
+    var viewheightPlaces = (height) - 365;
     // var advHeight = $(".advanced-view").show().height();
     // var comboHeight = (viewheight) - 370;
 
